553bd69facd3c0857725809dd5043c5c915cd731
nandoi0 <97204056+nandoi0@users.noreply.github.com>
2022-05-02 22:04:59 -0700

    FSDI_112c1

---
+++ pages/tests.py
@@ -0,0 +1,39 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contains_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Hello World!")
+
+    def test_about_page_uses_correct_templates(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_home_about_contains_content(self):
+        response = self.client.get("/about/")
+        self.assertContains(response, "Hi my name is Fernando")
+
+    def test_about_pages_uses_correct_templates(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, "about.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "about.html")
